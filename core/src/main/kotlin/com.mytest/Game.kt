6989319d6a255ee2149b9be7b56d84049cfc68f4
Stephen Dunn <dunn.stephen@gmail.com>
2019-06-23 13:48:00 -0700

    refactored to keep sample app minimal

--- core/src/main/kotlin/com.mytest/Game.kt
+++ core/src/main/kotlin/com.mytest/Game.kt
@@ -2,27 +2,12 @@ package com.mytest
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Screen
-import com.badlogic.gdx.assets.AssetManager
-import com.badlogic.gdx.assets.loaders.FileHandleResolver
-import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
-import com.badlogic.gdx.scenes.scene2d.Stage
-import com.badlogic.gdx.utils.viewport.Viewport
 import ktx.app.KtxGame
 import ktx.inject.Context
-import com.badlogic.gdx.assets.loaders.SkinLoader
-import com.badlogic.gdx.assets.loaders.BitmapFontLoader
-import com.badlogic.gdx.graphics.Camera
-import com.badlogic.gdx.graphics.OrthographicCamera
-import com.badlogic.gdx.graphics.g2d.BitmapFont
-import com.badlogic.gdx.graphics.g2d.freetype.FreetypeFontLoader
-import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGeneratorLoader
-import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator
-import com.badlogic.gdx.scenes.scene2d.ui.Skin
-import com.badlogic.gdx.utils.viewport.ScreenViewport
 
 class Game : KtxGame<Screen>() {
     val context = Context()
@@ -31,17 +16,7 @@ class Game : KtxGame<Screen>() {
 
     override fun create() {
         context.register {
-            bindSingleton<FileHandleResolver>(InternalFileHandleResolver())
-            bindSingleton(AssetManager(inject()).apply {
-                setLoader(FreeTypeFontGenerator::class.java, FreeTypeFontGeneratorLoader(inject()))
-                setLoader(BitmapFont::class.java, ".ttf", FreetypeFontLoader(inject()))
-                setLoader(BitmapFont::class.java, ".fnt", BitmapFontLoader(inject()))
-                setLoader(Skin::class.java, ".json", SkinLoader(inject()))
-            })
-            bindSingleton<Camera>(OrthographicCamera())
             bindSingleton<Batch>(SpriteBatch())
-            bindSingleton<Viewport>(ScreenViewport(inject()))
-            bindSingleton(Stage(inject(), inject()))
         }
 
         img = Texture("assets/badlogic.jpg")
